a3e7c31a7d7566155f11dc681ef9f9f4b44ecf6d
Neschetnyi <a.neschetnyi@gmail.com>
2025-02-08 15:18:18 +0500

    fuck new-todo-form

--- src/components/TodoApp/Header/NewTaskForm/NewTaskForm.js
+++ src/components/TodoApp/Header/NewTaskForm/NewTaskForm.js
@@ -23,7 +23,7 @@ class NewTaskForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form onSubmit={this.onSubmit} className="new-todo-form">
         <input
           className="new-todo"
           placeholder="Что сделать?"
