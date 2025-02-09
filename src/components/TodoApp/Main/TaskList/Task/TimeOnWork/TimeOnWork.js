97c06057ac3991677b57f6051c4a97316d3079c7
Neschetnyi <a.neschetnyi@gmail.com>
2025-02-09 22:47:36 +0500

    add converter of seconds

--- src/components/TodoApp/Main/TaskList/Task/TimeOnWork/TimeOnWork.js
+++ src/components/TodoApp/Main/TaskList/Task/TimeOnWork/TimeOnWork.js
@@ -1,23 +1,66 @@
 import React, { Component } from "react";
 
 class TimeOnWork extends Component {
-  state = {
-    day: this.props.day,
-    hour: this.props.hour,
-    min: this.props.min,
-    sec: this.props.sec,
+  secondsConverter = (seconds = 0) => {
+    let ResultSec = seconds;
+    let dayF = (ResultSec - (ResultSec % 86400)) / 86400;
+    let hourMinSec = ResultSec - dayF * 86400;
+    let hourMinSecOst = hourMinSec % 3600;
+    let hoursInSec = hourMinSec - hourMinSecOst;
+    let hourF = hoursInSec / 3600;
+    let minSec = hourMinSec - hoursInSec;
+    let minSecOst = minSec % 60;
+    let minInSec = minSec - minSecOst;
+    let minF = minInSec / 60;
+    let SecF = ResultSec - dayF * 86400 - hourF * 3600 - minF * 60;
+
+    console.log("ResultSec:", ResultSec);
+    console.log("");
+    console.log("day:", dayF);
+    console.log("");
+    console.log("hour:", hourF);
+    console.log("");
+    console.log("min:", minF);
+    console.log("");
+    console.log("Sec:", SecF);
+    return { dayF, hourF, minF, SecF, ResultSec };
   };
 
   componentDidMount() {
-    /* this.timer = setInterval(() => {
-      this.props.changeTimeState(this.props.id, {
-        day: this.props.day,
-        hour: this.props.hour,
-        min: this.props.min,
-        sec: this.props.sec,
-      });
-    }, 1000);*/
+    let time = this.secondsConverter(this.props.timeInSec);
+    this.setState({
+      day: time.dayF,
+      hour: time.hourF,
+      min: time.minF,
+      sec: time.SecF,
+      timeInSec: time.ResultSec,
+    });
+    this.timer = setInterval(() => {
+      let newTimeINSec = this.state.timeInSec - 1;
+      this.props.changeTimeState(this.props.id, newTimeINSec);
+    }, 1000);
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.timeInSec !== this.props.timeInSec) {
+      let time = this.secondsConverter(this.props.timeInSec);
+      this.setState({
+        day: time.dayF,
+        hour: time.hourF,
+        min: time.minF,
+        sec: time.SecF,
+        timeInSec: time.ResultSec,
+      });
+    }
+  }
+
+  state = {
+    day: "",
+    hour: "",
+    min: "",
+    sec: "",
+    timeInSec: "",
+  };
 
   render() {
     console.log("Props in timerOnWork", this.props);
@@ -25,9 +68,7 @@ class TimeOnWork extends Component {
       <div class="description">
         <button class="icon icon-play"></button>
         <button class="icon icon-pause"></button>
-        {((this.props.timeInSec % 86400) % 3600) % 60}:
-        {(this.props.timeInSec % 86400) % 3600}:{this.props.timeInSec % 86400}:
-        {this.props.timeInSec}:
+        {this.state.day}:{this.state.hour}:{this.state.min}:{this.state.sec}
       </div>
     );
   }
