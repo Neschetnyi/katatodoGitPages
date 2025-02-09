e9ec5dd73ad03e86a28075b4bef9600643957f9f
Neschetnyi <a.neschetnyi@gmail.com>
2025-02-09 20:21:46 +0500

    1

--- src/components/TodoApp/Main/TaskList/Task/TimeOnWork/TimeOnWork.js
+++ src/components/TodoApp/Main/TaskList/Task/TimeOnWork/TimeOnWork.js
@@ -25,8 +25,10 @@ class TimeOnWork extends Component {
       <div class="description">
         <button class="icon icon-play"></button>
         <button class="icon icon-pause"></button>
-        {this.props.timeInSec % 86400}:{this.props.timeInSec % 3600}:
-        {this.props.timeInSec % 60}:{this.props.timeInSec}
+        {(this.props.timeInSec - (this.props.timeInSec % 86400)) / 86400}:
+        {(this.props.timeInSec - (this.props.timeInSec % 3600)) / 3600}:
+        {(this.props.timeInSec - (this.props.timeInSec % 60)) / 60}:
+        {this.props.timeInSec}
       </div>
     );
   }
